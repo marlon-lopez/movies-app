2038755fbc280b0ac5cdc29abf4e70932e3dd339
marlon-lopez <marlonlopez5647@gmail.com>
2022-05-09 18:32:12 -0600

    update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-blue': '#202026',
+        'secondary-blue': '#313A47',
+        'tertiary-blue': '#34445B',
+      },
+      gridTemplateColumns: {
+        'auto-fit': 'repeat(auto-fit, minmax(160px, 1fr))',
+      },
+    },
   },
   variants: {
     extend: {},
